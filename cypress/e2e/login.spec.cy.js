16ded59ee6151b0e909a0756f38aaa39c4dff599
Jorge Teixeira II <jorgetxjr@gmail.com>
2026-01-15 22:27:21 -0300

    Creating Fixtures, in JSON

--- cypress/e2e/login.spec.cy.js
+++ cypress/e2e/login.spec.cy.js
@@ -1,3 +1,5 @@
+import userData from "../fixtures/user-data.json"
+
 describe('Orange HRM Tests', () => {
   const selectorsList={
     urlTarget:'https://opensource-demo.orangehrmlive.com/web/index.php/auth/login', 
@@ -5,20 +7,22 @@ describe('Orange HRM Tests', () => {
     passwordField:'[name="password"]',
     loginButton:'.oxd-button',
     sectionTitleTopBar: '.oxd-topbar-header-breadcrumb > .oxd-text',
+    dashboadGrid: ".orangehrm-dashboard-grid",
     credentialWrong:'.oxd-alert-content > .oxd-text',
   }
+
   it('Login - sucess', () => {
     cy.visit(selectorsList.urlTarget)
-    cy.get(selectorsList.usernameField).type("Admin")
-    cy.get(selectorsList.passwordField).type("admin123")
+    cy.get(selectorsList.usernameField).type(userData.validUser)
+    cy.get(selectorsList.passwordField).type(userData.validPassword)
     cy.get(selectorsList.loginButton).click()
     cy.location("pathname").should("equal","/web/index.php/dashboard/index")
-    cy.get(selectorsList.sectionTitleTopBar).contains("Dashboard")
+    cy.get(selectorsList.dashboadGrid)
   })
   it('Login - Fail',()=>{
     cy.visit(selectorsList.urlTarget)
-    cy.get(selectorsList.usernameField).type("Test")
-    cy.get(selectorsList.passwordField).type("test")
+    cy.get(selectorsList.usernameField).type(userData.invalidUser)
+    cy.get(selectorsList.passwordField).type(userData.invalidPassword)
     cy.get(selectorsList.loginButton).click()
     cy.get(selectorsList.credentialWrong)
   })
