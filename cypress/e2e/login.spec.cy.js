0765651ad58765325eecc35f69b71155fb7e2de2
Jorge Teixeira II <jorgetxjr@gmail.com>
2026-01-15 21:40:11 -0300

    Putting all 'get targets' in a single constant

--- cypress/e2e/login.spec.cy.js
+++ cypress/e2e/login.spec.cy.js
@@ -1,17 +1,25 @@
 describe('Orange HRM Tests', () => {
+  const selectorsList={
+    urlTarget:'https://opensource-demo.orangehrmlive.com/web/index.php/auth/login', 
+    usernameField:'[name="username"]',
+    passwordField:'[name="password"]',
+    loginButton:'.oxd-button',
+    sectionTitleTopBar: '.oxd-topbar-header-breadcrumb > .oxd-text',
+    credentialWrong:'.oxd-alert-content > .oxd-text',
+  }
   it('Login - sucess', () => {
-    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-    cy.get('[name="username"]').type("Admin")
-    cy.get('[name="password"]').type("admin123")
-    cy.get('.oxd-button').click()
+    cy.visit(selectorsList.urlTarget)
+    cy.get(selectorsList.usernameField).type("Admin")
+    cy.get(selectorsList.passwordField).type("admin123")
+    cy.get(selectorsList.loginButton).click()
     cy.location("pathname").should("equal","/web/index.php/dashboard/index")
-    cy.get('.oxd-topbar-header-breadcrumb > .oxd-text').contains("Dashboard")
+    cy.get(selectorsList.sectionTitleTopBar).contains("Dashboard")
   })
   it('Login - Fail',()=>{
-    cy.visit("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
-    cy.get('[name="username"]').type("test")
-    cy.get('[name="password"]').type("test")
-    cy.get('.oxd-button').click()
-    cy.get('.oxd-alert-content > .oxd-text')
+    cy.visit(selectorsList.urlTarget)
+    cy.get(selectorsList.usernameField).type("Test")
+    cy.get(selectorsList.passwordField).type("test")
+    cy.get(selectorsList.loginButton).click()
+    cy.get(selectorsList.credentialWrong)
   })
 })
